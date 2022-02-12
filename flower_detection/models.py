f3fdc117d586704abb0d109fc0d4b7d987b81e5c
Tejas-15 <tkadam252@gmail.com>
2022-02-12 12:06:11 +0530

    12/02/2022

--- flower_detection/models.py
+++ flower_detection/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Info(models.Model):
+    Flower_name = models.CharField(max_length=50) 
+    Introduction = models.TextField()
+    Etymology = models.TextField()
+    Evolution = models.TextField()
+    Species = models.TextField()
+    Taxonomy = models.TextField()
+    Ornamental_plants = models.TextField()
+    Cut_flowers = models.TextField()
+    Medicinal_uses = models.TextField()
+    Health_benefit = models.TextField()
+    Perfumes_food = models.TextField()
+    Symbolizes = models.TextField()
+    Season_to_grow = models.TextField()
+#     Image_flower = models.ImageField(null=True, blank=True, upload_to")
+
+    def __str__(self):
+        return self.Flower_name
+
+# class Upload_img(models.Model): 
+#     Uploaded_image = models.ImageField() 
--- flower_detection/urls.py
+++ flower_detection/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home , name='main_home'),
-    path('upload/', views.upload , name='upload'),
-    path('test/', views.test , name='test_home'),
+    # path('upload/', views.upload , name='upload'),
+    path('About/', views.About , name='About'),
     path('daisy/', views.daisy , name='daisy'),
     path('yellow_rose/', views.yellow_rose , name='yellow_rose'),
-    path('pink_rose/', views.pink_rose , name='pink_rose'),
-    path('Test1/', views.Test1 , name='Test1'),
+    path('rose/', views.rose , name='rose'),
+    path('hometest/', views.hometest , name='hometest'),
+    path('Test1/', views.flower_info , name='Test1'),
+
     
 ]
